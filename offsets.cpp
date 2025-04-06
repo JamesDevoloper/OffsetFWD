cb03a69bd04321e423b299e140d61384882e7d21
JamesDevoloper <154053203+JamesDevoloper@users.noreply.github.com>
2025-04-06 22:06:30 +0300

    Create offsets.cpp

---
+++ offsets.cpp
@@ -0,0 +1,17 @@
+FWD 1.2047
+
+0x10034F4 radar hack
+0xFF76A4 ammo
+0xFFE094 health 
+0x109F348 plant
+0x9A9018 fov
+0xB7ED90 KD
+0xB2FE5C WIN
+For ESP
+0x268B2BC position 
+0x25EBF38 transform 
+0xD9FB6C camera
+0x2676EBC WorldToScreenPoint(Vector3 position)
+0x2676F2C Vector3 WorldToScreenPoint
+
+Good luck!
